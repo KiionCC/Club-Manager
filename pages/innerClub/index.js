3df5fd42a2ad7fba563d99a43c00f8e3cc6bc538
杨财辉 <1017730056@qq.com>
2019-01-09 20:26:33 +0800

    解决部分机型不能加载出社团功能处图片的问题

--- pages/innerClub/index.js
+++ pages/innerClub/index.js
@@ -21,13 +21,13 @@ Page({
 
     //功能
     memFunc: [
-      { name: "查看成员" },
-      { name: "成员统计", url: '../statistical/index' }
+      { name: "查看成员", icon: "member" },
+      { name: "成员统计", icon:"memberStatic", url: '../statistical/index' }
     ],
     adminFunc: [
-      { name: "发起活动" }, 
-      { name: "发起投票", url:'../createVoting/index' }, 
-      { name:"处理申请", url:'../handleJoin/index' },
+      { name: "发起活动", icon:"releaseEvent" }, 
+      { name: "发起投票", icon: "releaseVote", url:'../createVoting/index' }, 
+      { name: "处理申请", icon: "handleAppli",url:'../handleJoin/index' },
     ],
   },
 
